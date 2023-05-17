561612454a03ff22fb69b182398d6a79db21fea7
E094746rem <99578532+E094746rem@users.noreply.github.com>
2023-05-17 03:35:51 -0700

    Update quze-script.js
    This is a JavaScript function named “loadQuiz” that displays the current quiz question and answer options. It first calls the “deselectAnswers” function to clear any previous selection,

--- quizeAPP/quze-script.js
+++ quizeAPP/quze-script.js
@@ -99,3 +99,16 @@ const getSelected = () => {
   });
   return answer;
 };
+const loadQuiz = () => {
+  deselectAnswers();
+  const currentQuizData = quizData[currentQuiz];
+  questionElement.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+};
+
+loadQuiz();
+
+
