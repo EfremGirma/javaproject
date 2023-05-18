b990f29595549aa1a781c9af7b58ed7d244403e5
E094746rem <99578532+E094746rem@users.noreply.github.com>
2023-05-17 22:54:08 -0700

    Update index.js
    telling the hour, minute and every one else where to start by using a function method

--- Stoptimer/index.js
+++ Stoptimer/index.js
@@ -8,3 +8,17 @@ document.getElementById("start-timer").addEventListener("click", () => {
     }
     int = setInterval(displayTimer, 10);
 });
+function displayTimer() {
+    milliseconds += 10;
+    seconds = milliseconds == 1000 ? (seconds + 1) % 60 : seconds;
+    minutes = seconds == 0 && milliseconds == 0 ? (minutes + 1) % 60 : minutes;
+    hours = minutes == 0 && seconds == 0 && milliseconds == 0 ? hours + 1 : hours;
+    milliseconds = milliseconds == 1000 ? 0 : milliseconds;
+
+    let h = String(hours).padStart(2, "0");
+    let m = String(minutes).padStart(2, "0");
+    let s = String(seconds).padStart(2, "0");
+    let ms = String(milliseconds).padStart(3, "0");
+
+    refrence.innerHTML = `${h} : ${m} : ${s} : ${ms}`;
+}
