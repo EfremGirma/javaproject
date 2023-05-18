e1b02684ede5bcd7cf9eb749f2d64c193cc49969
E094746rem <99578532+E094746rem@users.noreply.github.com>
2023-05-17 22:52:45 -0700

    Update index.js
    creating a functionality to the time displayer and adding some functionality to the start timer

--- Stoptimer/index.js
+++ Stoptimer/index.js
@@ -1,1 +1,10 @@
+let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+let refrence = document.querySelector(".timer-display");
+let int = null;
 
+document.getElementById("start-timer").addEventListener("click", () => {
+    if (int !== null) {
+        clearInterval(int);
+    }
+    int = setInterval(displayTimer, 10);
+});
